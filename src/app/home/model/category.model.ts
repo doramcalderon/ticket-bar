d8c4715d9858aea5b181949e863a24b97afcdf00
Adoracion Calderon <acalderonriv@csc.com>
2020-01-26 19:59:06 +0100

    feat(caegories): ticket types

--- src/app/home/model/category.model.ts
+++ src/app/home/model/category.model.ts
@@ -2,4 +2,10 @@ export interface Category {
     name: string;
     icon?: string;
     color?: string;
+    tickets?: TicketType[];
+}
+
+export interface TicketType {
+    name: string;
+    price: number;
 }
