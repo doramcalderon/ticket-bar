bb617b2d82e308e18b99fe04a6962e4a0c06a8ef
Adoracion Calderon <acalderonriv@csc.com>
2020-01-26 19:34:41 +0100

    feat(home): load categories in Home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Category } from './model/category.model';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+    public categories: Array<Category> = [
+        { name: 'Bebida', icon: 'beer', color: 'food' },
+        { name: 'Comida', icon: 'pizza', color: 'drink' },
+        { name: 'Otros', icon: '', color: 'warning' },
+    ];
 
-  constructor() {}
+    constructor() {}
 
+    ngOnInit() {}
 }
