26f8b4d07cc89672aea9bcb3a5cfd23743369985
Adoracion Calderon <acalderonriv@csc.com>
2020-02-01 20:52:39 +0100

    refactor(home): create home routing module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,23 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
 import { CategoryButtonComponent } from './components/category-button/category-button.component';
+import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        FormsModule,
-        IonicModule,
-        RouterModule.forChild([
-            {
-                path: '',
-                component: HomePage,
-            },
-        ]),
-    ],
     declarations: [HomePage, CategoryButtonComponent],
+    imports: [CommonModule, FormsModule, IonicModule, HomeRoutingModule],
 })
 export class HomePageModule {}
