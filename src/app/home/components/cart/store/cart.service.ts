4aca38ccd3221570836e3bf27f2a5c718bed8254
Adoracion Calderon <acalderonriv@csc.com>
2020-02-04 20:27:24 +0100

    feat(cart): add cart store service

---
+++ src/app/home/components/cart/store/cart.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { TicketType } from '../../../../common/model/category.model';
+import * as CartActions from './cart.actions';
+import { Cart } from './cart.model';
+import * as CartSelectors from './cart.selectors';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class CartService {
+    constructor(private store: Store<Cart>) {}
+
+    public loadCart() {
+        this.store.dispatch(CartActions.loadCart());
+    }
+
+    public addTicket(ticket: TicketType) {
+        this.store.dispatch(CartActions.addTicket({ ticket }));
+    }
+
+    public getSelectCartTickets(): Observable<TicketType[]> {
+        return this.store.select(CartSelectors.selectCartTickets);
+    }
+}
