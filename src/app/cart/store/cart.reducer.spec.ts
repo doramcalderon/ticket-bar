5d81e5bdf61a48d5d6b6b0127a1631ed4882c1c3
Adoracion Calderon <acalderonriv@csc.com>
2020-02-05 22:25:16 +0100

    test(cart): add `addTicket` action test

--- src/app/cart/store/cart.reducer.spec.ts
+++ src/app/cart/store/cart.reducer.spec.ts
@@ -1,13 +1,24 @@
-import { reducer, initialState } from './cart.reducer';
+import { reducer, initialState, CartState } from './cart.reducer';
+import * as CartActions from './cart.actions';
 
 describe('Cart Reducer', () => {
-  describe('an unknown action', () => {
-    it('should return the previous state', () => {
-      const action = {} as any;
+    describe('an unknown action', () => {
+        it('should return the previous state', () => {
+            const action = {} as any;
 
-      const result = reducer(initialState, action);
+            const result = reducer(initialState, action);
 
-      expect(result).toBe(initialState);
+            expect(result).toBe(initialState);
+        });
     });
-  });
+
+    describe('`[Cart] Add Ticket action`', () => {
+        it('should return the state with the ticket selected added', () => {
+            const state: CartState = { cart: { tickets: [] } };
+            const addTicketAction = CartActions.addTicket({ ticket: { name: 'foo', price: 4 } });
+            const newState: CartState = reducer(state, addTicketAction);
+
+            expect(newState).toEqual({ cart: { tickets: [{ name: 'foo', price: 4 }] } });
+        });
+    });
 });
