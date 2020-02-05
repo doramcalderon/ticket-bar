905ae7ee1c427a544e2499410aa04ebe27e5eec2
Adoracion Calderon <acalderonriv@csc.com>
2020-02-05 22:12:46 +0100

    test(cart): add cart selectors test

--- src/app/cart/store/cart.selectors.spec.ts
+++ src/app/cart/store/cart.selectors.spec.ts
@@ -1,12 +1,27 @@
+import { TicketType } from '../../common/model/category.model';
 import * as fromCart from './cart.reducer';
-import { selectCartState } from './cart.selectors';
+import { selectCartState, selectCartTickets, selectCartTotal } from './cart.selectors';
 
 describe('Cart Selectors', () => {
     it('should select the feature state', () => {
         const result = selectCartState({
             [fromCart.cartFeatureKey]: {},
         });
-        console.log(`result: ${JSON.stringify(result)}`);
         expect(result).toEqual({} as fromCart.CartState);
+    });
+
+    it('should select the cart tickets', () => {
+        const tickets: TicketType[] = selectCartTickets({ cart: { cart: { tickets: [] } } });
+        expect(tickets).toEqual([]);
+    });
+
+    it('should select the cart tickets length when there is not tickets in the cart', () => {
+        const total: number = selectCartTotal({ cart: { cart: { tickets: [] } } });
+        expect(total).toEqual(0);
+    });
+
+    it('should select the cart tickets length when there is tickets in the cart', () => {
+        const total: number = selectCartTotal({ cart: { cart: { tickets: [{ name: 'foo', price: 3 }] } } });
+        expect(total).toEqual(1);
     });
 });
