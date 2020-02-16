bd3caa10bae60aafd40cc6baf7cc700a2a53c5ef
Adoracion Calderon <acalderonriv@csc.com>
2020-02-16 20:31:19 +0100

    refactor(home): improve Home content

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -3,16 +3,31 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { HomePage } from './home.page';
+import { CategoriesService } from '../common/services/categories.service';
+import { CartService } from '../cart/store/cart.service';
+import { TicketType } from '../common/model/category.model';
 
 describe('HomePage', () => {
     let component: HomePage;
     let fixture: ComponentFixture<HomePage>;
+
+    const categoriesServiceStub = {
+        getAllCategories: () => [],
+    };
+
+    const cartServiceStub = {
+        getTicketsTypeCount: (t: TicketType): number => 0,
+    };
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [HomePage],
             imports: [IonicModule.forRoot()],
             schemas: [NO_ERRORS_SCHEMA],
+            providers: [
+                { provide: CategoriesService, useValue: categoriesServiceStub },
+                { provide: CartService, useValue: cartServiceStub },
+            ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(HomePage);
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../common/model/category.model';
+import { CategoriesService } from '../common/services/categories.service';
+
+import { Observable } from 'rxjs';
+
+import { CartService } from '../cart/store/cart.service';
 
 @Component({
     selector: 'app-home',
@@ -6,7 +12,28 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-    constructor() {}
+    public categories: Category[];
+    public categoriesSelected: Category[];
+    ticketsCountByType$: { [type: string]: Observable<number> } = {};
 
-    ngOnInit() {}
+    constructor(private categoriesService: CategoriesService, private cartService: CartService) {}
+
+    ngOnInit() {
+        this.categories = this.categoriesService.getAllCategories();
+        this.categoriesSelected = this.categories;
+        this.categories.forEach(c => this.initSummary(c));
+    }
+
+    filterCategories(categoriesFilter: Category[]): void {
+        const categoriesId: string[] = categoriesFilter.map(c => c.id);
+        this.categoriesSelected = this.categories.filter(c => !!categoriesId.find(id => id === c.id));
+    }
+
+    private initSummary(category: Category) {
+        if (!!category.tickets) {
+            category.tickets.forEach(ticketType => {
+                this.ticketsCountByType$[ticketType.name] = this.cartService.getTicketsTypeCount(ticketType);
+            });
+        }
+    }
 }
