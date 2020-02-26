1b15b238367a72fe64dcb4361c554877a4dd2b8b
Adoracion Calderon <acalderonriv@csc.com>
2020-02-26 21:59:59 +0100

    feat(cart): print cart tickets button

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -3,21 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
-import { StoreModule } from '@ngrx/store';
+import { NgxPrintModule } from 'ngx-print';
 
-import * as fromCart from '../cart/store/cart.reducer';
 import { CartPageRoutingModule } from './cart-routing.module';
 import { CartPage } from './cart.page';
 import { TicketComponent } from './components/ticket/ticket.component';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        FormsModule,
-        IonicModule,
-        StoreModule.forFeature(fromCart.cartFeatureKey, fromCart.reducer),
-        CartPageRoutingModule,
-    ],
+    imports: [CommonModule, FormsModule, IonicModule, NgxPrintModule, CartPageRoutingModule],
     declarations: [CartPage, TicketComponent],
 })
 export class CartPageModule {}
