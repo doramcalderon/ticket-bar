139d00805284a758b8fbd6faa99c9b83f22db2ca
Adoracion Calderon <acalderonriv@csc.com>
2020-02-26 21:58:19 +0100

    refactor(cart): convert cart component to page

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
+import { StoreModule } from '@ngrx/store';
+
+import * as fromCart from '../cart/store/cart.reducer';
 import { TicketBarCommonModule } from '../common/ticket-bar-common.module';
 import { CategoriesListComponent } from './components/categories-list/categories-list.component';
 import { CategoryButtonComponent } from './components/category-button/category-button.component';
@@ -11,7 +14,14 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomePage } from './home.page';
 
 @NgModule({
-    imports: [CommonModule, FormsModule, IonicModule, HomeRoutingModule, TicketBarCommonModule],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        HomeRoutingModule,
+        TicketBarCommonModule,
+        StoreModule.forFeature(fromCart.cartFeatureKey, fromCart.reducer),
+    ],
     declarations: [HomePage, CategoryButtonComponent, CategoriesListComponent, CategoryFilterComponent],
 })
 export class HomePageModule {}
