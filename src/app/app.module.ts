3b1aaeedc4493e43e169ccbc722d4ed154c7b38b
Adoracion Calderon <acalderonriv@csc.com>
2020-03-31 19:44:31 +0200

    build: install ionic storage plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicStorageModule } from '@ionic/storage';
 
-import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppEffects } from './app.effects';
 import { metaReducers, reducers } from './reducers';
-import { environment } from '../environments/environment';
 
 @NgModule({
     declarations: [AppComponent],
@@ -22,6 +23,7 @@ import { environment } from '../environments/environment';
     imports: [
         BrowserModule,
         IonicModule.forRoot(),
+        IonicStorageModule.forRoot(),
         AppRoutingModule,
         StoreModule.forRoot(reducers, {
             metaReducers,
