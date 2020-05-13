5ffcbbc59b3ed93a8216d82ade59b79bd03c670d
Adoracion Calderon <acalderonriv@csc.com>
2020-05-13 20:32:20 +0200

    feat(categories): add categories selector

--- src/app/config/categories-config/store/categories.selectors.spec.ts
+++ src/app/config/categories-config/store/categories.selectors.spec.ts
@@ -1,1 +1,34 @@
-describe('Cart Selectors', () => {});
+import { Category } from 'src/app/common/model/category.model';
+import { CategoriesState, categoriesFeatureKey } from './categories.reducer';
+import { selectCategories, selectCategoriesState } from './categories.selectors';
+
+describe('Cart Selectors', () => {
+    let state: CategoriesState;
+    beforeEach(() => {
+        state = {
+            categories: [
+                {
+                    id: 'Bebida',
+                    name: 'Bebida',
+                },
+                {
+                    id: 'Comida',
+                    name: 'Comida',
+                },
+            ],
+        };
+    });
+
+    it('should select the feature state', () => {
+        const result: CategoriesState = selectCategoriesState({ [categoriesFeatureKey]: state });
+        expect(result).toEqual(state);
+    });
+
+    describe('#selectCategories', () => {
+        it('should return the categories of the state', () => {
+            const expected: Category[] = state.categories;
+            const selection: Category[] = selectCategories.projector(state);
+            expect(selection).toEqual(expected);
+        });
+    });
+});
--- src/app/config/categories-config/store/categories.selectors.ts
+++ src/app/config/categories-config/store/categories.selectors.ts
@@ -1,5 +1,6 @@
-import { createFeatureSelector } from '@ngrx/store';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 
 import * as fromCategories from './categories.reducer';
 
 export const selectCategoriesState = createFeatureSelector<fromCategories.CategoriesState>(fromCategories.categoriesFeatureKey);
+export const selectCategories = createSelector(selectCategoriesState, (state: fromCategories.CategoriesState) => state.categories);
