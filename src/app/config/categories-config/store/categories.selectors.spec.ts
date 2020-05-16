f617844edf34e3b91503fdcdb28f2e2f2cc35a00
Adoracion Calderon <acalderonriv@csc.com>
2020-05-16 13:27:41 +0200

    feat(categories): create categories count selector

--- src/app/config/categories-config/store/categories.selectors.spec.ts
+++ src/app/config/categories-config/store/categories.selectors.spec.ts
@@ -1,6 +1,6 @@
 import { Category } from 'src/app/common/model/category.model';
 import { CategoriesState, categoriesFeatureKey } from './categories.reducer';
-import { selectCategories, selectCategoriesState } from './categories.selectors';
+import { selectCategories, selectCategoriesState, selectCategoriesCount } from './categories.selectors';
 
 describe('Cart Selectors', () => {
     let state: CategoriesState;
@@ -28,6 +28,14 @@ describe('Cart Selectors', () => {
         it('should return the categories of the state', () => {
             const expected: Category[] = state.categories;
             const selection: Category[] = selectCategories.projector(state);
+            expect(selection).toEqual(expected);
+        });
+    });
+
+    describe('#selectCategoriesCount', () => {
+        it('should return the number of categories of the state', () => {
+            const expected: number = state.categories.length;
+            const selection: number = selectCategoriesCount.projector(state.categories);
             expect(selection).toEqual(expected);
         });
     });
--- src/app/config/categories-config/store/categories.selectors.ts
+++ src/app/config/categories-config/store/categories.selectors.ts
@@ -1,6 +1,8 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 
 import * as fromCategories from './categories.reducer';
+import { Category } from 'src/app/common/model/category.model';
 
 export const selectCategoriesState = createFeatureSelector<fromCategories.CategoriesState>(fromCategories.categoriesFeatureKey);
 export const selectCategories = createSelector(selectCategoriesState, (state: fromCategories.CategoriesState) => state.categories);
+export const selectCategoriesCount = createSelector(selectCategories, (categories: Category[]) => (!!categories ? categories.length : 0));
