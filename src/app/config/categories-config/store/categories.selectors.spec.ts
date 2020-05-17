681a73a0be492a9bd479a7f15bbc5a2e360ad7e3
Adoracion Calderon <acalderonriv@csc.com>
2020-05-17 12:29:15 +0200

    feat(categories): tickets selectors

--- src/app/config/categories-config/store/categories.selectors.spec.ts
+++ src/app/config/categories-config/store/categories.selectors.spec.ts
@@ -1,6 +1,6 @@
-import { Category } from 'src/app/common/model/category.model';
-import { CategoriesState, categoriesFeatureKey } from './categories.reducer';
-import { selectCategories, selectCategoriesState, selectCategoriesCount } from './categories.selectors';
+import { Category, TicketType } from 'src/app/common/model/category.model';
+import { categoriesFeatureKey, CategoriesState } from './categories.reducer';
+import * as fromSelectors from './categories.selectors';
 
 describe('Cart Selectors', () => {
     let state: CategoriesState;
@@ -10,24 +10,44 @@ describe('Cart Selectors', () => {
                 {
                     id: 'Bebida',
                     name: 'Bebida',
+                    tickets: [
+                        {
+                            name: 'Refresco',
+                            price: 2,
+                        },
+                        {
+                            name: 'Cerveza',
+                            price: 2,
+                        },
+                    ],
                 },
                 {
                     id: 'Comida',
                     name: 'Comida',
+                    tickets: [
+                        {
+                            name: 'Pizza',
+                            price: 8,
+                        },
+                    ],
+                },
+                {
+                    id: 'Otros',
+                    name: 'Otros',
                 },
             ],
         };
     });
 
     it('should select the feature state', () => {
-        const result: CategoriesState = selectCategoriesState({ [categoriesFeatureKey]: state });
+        const result: CategoriesState = fromSelectors.selectCategoriesState({ [categoriesFeatureKey]: state });
         expect(result).toEqual(state);
     });
 
     describe('#selectCategories', () => {
         it('should return the categories of the state', () => {
             const expected: Category[] = state.categories;
-            const selection: Category[] = selectCategories.projector(state);
+            const selection: Category[] = fromSelectors.selectCategories.projector(state);
             expect(selection).toEqual(expected);
         });
     });
@@ -35,8 +55,71 @@ describe('Cart Selectors', () => {
     describe('#selectCategoriesCount', () => {
         it('should return the number of categories of the state', () => {
             const expected: number = state.categories.length;
-            const selection: number = selectCategoriesCount.projector(state.categories);
+            const selection: number = fromSelectors.selectCategoriesCount.projector(state.categories);
             expect(selection).toEqual(expected);
+        });
+    });
+
+    describe('#selectTickets', () => {
+        it('should return an empty array when there is not categories with tickets', () => {
+            state = {
+                categories: [
+                    {
+                        id: 'withoutTickets',
+                        name: 'withoutTickets',
+                    },
+                ],
+            };
+            const expected: TicketType[] = [];
+            const selection: TicketType[] = fromSelectors.selectTickets.projector(state.categories);
+            expect(selection).toEqual(expected);
+        });
+
+        it('should return the tickets of all categories of the state', () => {
+            const expected: TicketType[] = [
+                {
+                    name: 'Refresco',
+                    price: 2,
+                },
+                {
+                    name: 'Cerveza',
+                    price: 2,
+                },
+                {
+                    name: 'Pizza',
+                    price: 8,
+                },
+            ];
+            const selection: TicketType[] = fromSelectors.selectTickets.projector(state.categories);
+            console.log(`selection: ${JSON.stringify(selection)}`);
+            console.log(`selection.length: ${selection.length}`);
+            expect(selection).toEqual(expected);
+        });
+    });
+
+    describe('#selectTicketsCount', () => {
+        it('should return 0 when there is not categories with tickets', () => {
+            const selection: number = fromSelectors.selectTicketsCount.projector([]);
+            expect(selection).toEqual(0);
+        });
+
+        it('should return the tickets count of all categories of the state', () => {
+            const tickets: TicketType[] = [
+                {
+                    name: 'Refresco',
+                    price: 2,
+                },
+                {
+                    name: 'Cerveza',
+                    price: 2,
+                },
+                {
+                    name: 'Pizza',
+                    price: 8,
+                },
+            ];
+            const selection: number = fromSelectors.selectTicketsCount.projector(tickets);
+            expect(selection).toEqual(3);
         });
     });
 });
--- src/app/config/categories-config/store/categories.selectors.ts
+++ src/app/config/categories-config/store/categories.selectors.ts
@@ -1,7 +1,7 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 
 import * as fromCategories from './categories.reducer';
-import { Category } from 'src/app/common/model/category.model';
+import { Category, TicketType } from 'src/app/common/model/category.model';
 
 export const selectCategoriesState = createFeatureSelector<fromCategories.CategoriesState>(fromCategories.categoriesFeatureKey);
 export const selectCategories = createSelector(selectCategoriesState, (state: fromCategories.CategoriesState) => state.categories);
@@ -9,3 +9,8 @@ export const selectCategoriesCount = createSelector(selectCategories, (categorie
 export const selectCategory = createSelector(selectCategories, (categories: Category[], cat: Category) =>
     !!categories ? categories.find((c) => c.id === cat.name) : undefined,
 );
+
+export const selectTickets = createSelector(selectCategories, (categories: Category[]) =>
+    categories.reduce((acc: TicketType[], category) => (!!category.tickets ? acc.concat(category.tickets) : acc), []),
+);
+export const selectTicketsCount = createSelector(selectTickets, (tickets: TicketType[]) => (!!tickets ? tickets.length : 0));
