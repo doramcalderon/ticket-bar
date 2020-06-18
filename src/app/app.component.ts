061661dc5634dca446bf31a62b5d1ed4e32c34f9
Adoracion Calderon <acalderonriv@csc.com>
2020-06-18 18:31:41 +0200

    feat(app): general app labels translations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,18 +12,7 @@ import { TranslateService } from '@ngx-translate/core';
     styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-    public appPages = [
-        {
-            title: 'Korner Tickets',
-            url: '/home',
-            icon: 'home',
-        },
-        {
-            title: 'Ajustes',
-            url: '/config',
-            icon: 'settings',
-        },
-    ];
+    public appPages;
 
     constructor(
         private platform: Platform,
@@ -44,6 +33,24 @@ export class AppComponent {
         this.translate.setDefaultLang('es');
 
         // the lang to use, if the lang isn't available, it will use the current loader to get them
-        this.translate.use('es');
+        this.translate.use('es').subscribe(() => this.initializePages());
+
+        // this.initializePages();
+    }
+
+    private initializePages(): void {
+        const titles: string[] = this.translate.instant(['HOME.TITLE', 'CONFIG.TITLE']);
+        this.appPages = [
+            {
+                title: titles['HOME.TITLE'],
+                url: '/home',
+                icon: 'home',
+            },
+            {
+                title: titles['CONFIG.TITLE'],
+                url: '/config',
+                icon: 'settings',
+            },
+        ];
     }
 }
